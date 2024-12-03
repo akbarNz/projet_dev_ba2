7b837fad65f21684987e9b79916509dc0bb7ec34
Benjos____ <benjamin.wera1@gmail.com>
2024-12-03 15:33:39 +0100

    ajout couleur dominante dans la classe oeuvre

--- art_management.py
+++ art_management.py
@@ -3,46 +3,43 @@
 from datetime import datetime
 
 class Artiste:
-    def __init__(self, identite, bio, date_naissance, date_deces=None):
+    def __init__(self, identite, biographie, date_naissance, date_deces=None):
         self.identite = identite
-        self.bio = bio
-        self.date_naissance = self._convertir_date(date_naissance)
-        self.date_deces = self._convertir_date(date_deces) if date_deces else None
+        self.biographie = biographie
+        self.date_naissance = convertir_date(date_naissance)
+        self.date_deces = convertir_date(date_deces) if date_deces else None
         self.oeuvres = []
-
-    def _convertir_date(self, date_str):
-        try:
-            return datetime.strptime(date_str, "%Y-%m-%d")
-        except ValueError:
-            print("Format de date incorrect. Utilisez AAAA-MM-JJ.")
-            return None
-
     def __str__(self):
-        return f"Artiste: {self.identite}, Bio: {self.bio}"
+        return f"Artiste: {self.identite}, Bio: {self.biographie}"
+
 
 class Oeuvre:
-    def __init__(self, titre, description, artiste=None):
+    def __init__(self, titre, description, couleur_dominante,artiste=None):
         self.titre = titre
         self.description = description
         self.artiste = artiste
         if artiste:
-            artiste.oeuvres.append(self)
+            artiste.oeuvre.append(self.titre)
+        self.couleur_dominante = couleur_dominante
 
     def __str__(self):
         artiste_info = self.artiste.identite if self.artiste else "Inconnu"
-        return f"Oeuvre: {self.titre}, Artiste: {artiste_info}, Description: {self.description}"
+        return f"Oeuvre: {self.titre}, Artiste: {artiste_info}, Description: {self.description}, Couleur Dominante: {self.couleur_dominante}"
+
 
 class Collection:
     def __init__(self, nom):
         self.nom = nom
         self.oeuvres = []
 
-    def ajouter_oeuvre(self, oeuvre):
-        self.oeuvres.append(oeuvre)
+        def ajouter_oeuvre(self, oeuvre):
+            self.oeuvres.append(oeuvre)
+
 
     def __str__(self):
         oeuvres_titres = ', '.join([oeuvre.titre for oeuvre in self.oeuvres])
         return f"Collection: {self.nom} avec les œuvres: [{oeuvres_titres}]"
+
 
 class Exposition:
     def __init__(self, nom, date_debut, date_fin, collection):
@@ -66,3 +63,10 @@ def trouver_oeuvre_par_titre(oeuvres, titre):
         if oeuvre.titre.lower() == titre.lower():
             return oeuvre
     return None
+
+def convertir_date(date_str):
+    try:
+        return datetime.strptime(date_str, "%Y-%m-%d")
+    except ValueError:
+        print("Format de date incorrect. Utilisez AAAA-MM-JJ.")
+        return None
