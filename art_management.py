634bb7a57f3c2cede06a516affc0658f9513558d
Benjos____ <benjamin.wera1@gmail.com>
2024-12-09 11:03:11 +0100

    Update art_management.py

--- art_management.py
+++ art_management.py
@@ -31,26 +31,38 @@ class Artiste:
 
 
 class Oeuvre:
-    def __init__(self, titre, description, artiste=None):
+    def __init__(self, titre, description, artiste=None, couleur_dominante=None, courant=None):
         self.titre = titre
         self.description = description
         self.artiste = artiste
+        self.couleur_dominante = couleur_dominante
+        self.courant = courant
 
     def __str__(self):
         artiste_info = self.artiste.identite if self.artiste else "Inconnu"
-        return f"Oeuvre: {self.titre}, Artiste: {artiste_info}, Description: {self.description}"
+        return (f"Oeuvre: {self.titre}, Artiste: {artiste_info}, Description: {self.description}, "
+                f"Couleur Dominante: {self.couleur_dominante}, Courant: {self.courant}")
 
     def to_dict(self):
         return {
             "titre": self.titre,
             "description": self.description,
-            "artiste": self.artiste.identite if self.artiste else None
+            "artiste": self.artiste.identite if self.artiste else None,
+            "couleur_dominante": self.couleur_dominante,
+            "courant": self.courant
         }
 
     @staticmethod
     def from_dict(data, artistes):
         artiste = next((a for a in artistes if a.identite == data["artiste"]), None)
-        return Oeuvre(data["titre"], data["description"], artiste)
+        return Oeuvre(
+            titre=data["titre"],
+            description=data["description"],
+            artiste=artiste,
+            couleur_dominante=data.get("couleur_dominante"),
+            courant=data.get("courant")
+        )
+
 
 
 class Collection:
