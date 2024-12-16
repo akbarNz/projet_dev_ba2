4ebf91e864948cbe517980b9fe6f84c018d5977c
Benjos____ <benjamin.wera1@gmail.com>
2024-12-16 20:17:30 +0100

    Plus de print et de dead code

--- ProjetDev/Oeuvre.py
+++ ProjetDev/Oeuvre.py
@@ -1,7 +1,3 @@
-import json
-import re
-
-
 class Oeuvre:
     def __init__(self, titre, description, artiste=None, couleur_dominante=None, courant=None):
         self.titre = titre
@@ -15,8 +11,6 @@ class Oeuvre:
         return (f"Oeuvre: {self.titre}, Artiste: {artiste_info}, Description: {self.description}, "
                 f"Couleur Dominante: {self.couleur_dominante}, Courant: {self.courant}")
 
-    def assigner_artiste(self, artiste):
-        self.artiste = artiste
     def to_dict(self):
         return {
             "titre": self.titre,
@@ -29,10 +23,7 @@ class Oeuvre:
     def assigner_artiste(self, artiste):
         if self.artiste is None:
             self.artiste = artiste
-            print(f"Artiste '{artiste.identite}' assigné à l'œuvre '{self.titre}'.")
-        else:
-            print(f"L'œuvre '{self.titre}' a déjà un artiste assigné : {self.artiste.identite}.")
-
+            
 
     @staticmethod
     def from_dict(data, artistes):
