bd49474e5fd616e564c350cc4650bae1b7edc78a
Benjos____ <benjamin.wera1@gmail.com>
2024-12-16 20:58:20 +0100

    ajout de l'encapsulation pour la class collection

--- ProjetDev/Collection.py
+++ ProjetDev/Collection.py
@@ -1,26 +1,38 @@
 class Collection:
     def __init__(self, nom):
-        self.nom = nom
-        self.oeuvres = []
+        self._nom = nom
+        self._oeuvres = []
+
+    @property
+    def nom(self):
+        return self._nom
+
+    @nom.setter
+    def nom(self, nom):
+        self._nom = nom
+
+    @property
+    def oeuvres(self):
+        return self._oeuvres
 
     def ajouter_oeuvre(self, oeuvre):
-        if oeuvre not in self.oeuvres:
-            self.oeuvres.append(oeuvre)
+        if oeuvre not in self._oeuvres:
+            self._oeuvres.append(oeuvre)
 
     def __str__(self):
-        oeuvres_titres = ', '.join([oeuvre.titre for oeuvre in self.oeuvres])
-        return f"Collection: {self.nom} avec les oeuvres: [{oeuvres_titres}]"
+        oeuvres_titres = ', '.join([oeuvre.titre for oeuvre in self._oeuvres])
+        return f"Collection: {self._nom} avec les oeuvres: [{oeuvres_titres}]"
 
     def to_dict(self):
         return {
-            "nom": self.nom,
-            "oeuvres": [oeuvre.titre for oeuvre in self.oeuvres]
+            "nom": self._nom,
+            "oeuvres": [oeuvre.titre for oeuvre in self._oeuvres]
         }
-    
+
     def enlever_oeuvre(self, oeuvre):
-        if oeuvre in self.oeuvres:
-            self.oeuvres.remove(oeuvre)
-            
+        if oeuvre in self._oeuvres:
+            self._oeuvres.remove(oeuvre)
+
     @staticmethod
     def from_dict(data, oeuvres):
         collection = Collection(data["nom"])
